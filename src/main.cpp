974eafd1b0e36f58e0e1961d9b68716e64bca6c8
foxcoder-0805 <eliseylomtev@gmail.com>
2026-05-08 16:13:25 +0300

    added open if args is empty

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,17 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    string search = "https://www.youtube.com/results?search_query=";
+    if(argc < 2) {
+        system("xdg-open https://www.youtube.com");
+    } else {
+        string search = "https://www.youtube.com/results?search_query=";
 
-    search.append(argv[1]);
+        search.append(argv[1]);
     
-    string bl = "xdg-open ";bl.append(search);
+        string bl = "xdg-open ";bl.append(search);
 
-    system(bl.c_str());
+        system(bl.c_str());
+    }
 
     return 0;
 }
