25597240ec645c46c9f9e011709a96106f9ca70f
foxcoder-0805 <eliseylomtev@gmail.com>
2026-05-09 14:54:56 +0300

    added -m

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,27 @@ int main(int argc, char* argv[]) {
     if(argc < 2) {
         system("xdg-open https://www.youtube.com");
     } else {
-        string search = "https://www.youtube.com/results?search_query=";
+        if(string(argv[1]) != "-m") {
+            string search = "https://www.youtube.com/results?search_query=";
 
-        search.append(argv[1]);
+            search.append(argv[1]);
     
-        string bl = "xdg-open ";bl.append(search);
+            string bl = "xdg-open ";
+            bl.append(search);
 
-        system(bl.c_str());
+            system(bl.c_str());
+        } else {
+            if(argc < 3) {
+                system("xdg-open https://music.youtube.com");
+            } else {
+                string search = "https://music.youtube.com/search?q=";
+                search.append(argv[2]);
+
+                string bl = "xdg-open ";bl.append(search);
+
+                system(bl.c_str());
+            }
+        }
     }
 
     return 0;
